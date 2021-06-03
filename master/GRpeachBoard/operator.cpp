27d2101f59c6935a4602e0a6ee9e527b6912c957
菊池隼 <e51190405b@edu.teu.ac.jp>
2021-06-03 12:49:38 +0900

    マイコン間通信の処理を追加

--- master/GRpeachBoard/operator.cpp
+++ master/GRpeachBoard/operator.cpp
@@ -1,9 +1,10 @@
 #include "operator.h"
 
-Operator::Operator()
+Operator::Operator(HardwareSerial *_upper)
 {
-    Operator::setup();
-    Operator::allOutputLow();
+  upper = _upper;
+  Operator::setup();
+  Operator::allOutputLow();
 }
 
 void Operator::setup()
@@ -15,11 +16,12 @@ void Operator::setup()
   pinMode(PIN_LED_2, OUTPUT);
   pinMode(PIN_LED_3, OUTPUT);
   pinMode(PIN_LED_4, OUTPUT);
-  pinMode(PIN_LED_ENC,OUTPUT);
+  pinMode(PIN_LED_ENC, OUTPUT);
 }
 
 // LEDをチカチカさせるための関数
-void Operator::LEDblink(byte pin, int times, int interval){
+void Operator::LEDblink(byte pin, int times, int interval)
+{
   analogWrite(pin, 0);
   for(int i = 0; i < times; i++){
     delay(interval);
@@ -29,7 +31,8 @@ void Operator::LEDblink(byte pin, int times, int interval){
   }
 }
 
-void Operator::RGB_led(int period){
+void Operator::RGB_led(int period)
+{
   // RGB LED を良い感じに光らせるための処理
   static int count = 0;
   count += period; // ここで光る周期を変えられる(はず)
@@ -57,4 +60,51 @@ void Operator::allOutputLow()
   digitalWrite(PIN_LED_3, LOW);
   digitalWrite(PIN_LED_4, LOW);
   digitalWrite(PIN_LED_ENC, LOW);
+}
+
+void Operator::sendUpperCmd()
+{
+  sendData[0] = 1; //ダミー
+  sendData[1] = 2; //ダミー
+  sendData[2] = 3; //ダミー
+  sendData[3] = 4; //ダミー
+  sendData[4] = 5; //ダミー
+  sendData[5] = (sendData[0] ^ sendData[1] ^ sendData[2] ^ sendData[3] ^ sendData[4]);
+  sendData[6] = END_BYTE;
+
+  for (int i = 0; i < 7; i++)
+  {
+    upper->write(sendData[i]);
+  }
+  
+}
+
+void Operator::updateUpperCmd(uint8_t *status)
+{
+
+  while (upper->available())
+  {
+    uint8_t num = upper->read();
+    static int loop_num = 0;
+    if(num == END_BYTE)
+    {
+      if(recv_num[2] == (recv_num[0] ^ recv_num[1]))
+      {
+        for (int i = 0; i < 4; i++)
+        {
+          reciveData[i] = recv_num[i];
+        }
+      }
+
+      *status = reciveData[0];
+
+      loop_num = 0;
+    }
+    else
+    {
+      recv_num[loop_num] = num;
+      loop_num++;
+    }
+  }
+  
 }
--- master/GRpeachBoard/operator.h
+++ master/GRpeachBoard/operator.h
@@ -4,19 +4,31 @@
 #include <Arduino.h>
 #include "define.h"
 
+#define END_BYTE 0xB4
+
 class Operator
 {
 public:
     /*********** 変数宣言 ***********/
 
     /*********** 関数宣言 ***********/
-    Operator();
+    Operator(HardwareSerial *_upper);
     void setup(void); //DRに関するセットアップを行う
     void allOutputLow(void); //全てのデジタル出力をLOWにする
     void LEDblink(byte pin, int times, int interval); //LEDを点滅させる
     void RGB_led(int period); //フルカラーLEDを奇麗に光らせる
 
+    void sendUpperCmd();
+    void updateUpperCmd(uint8_t *status);
+
+    uint8_t sendData[7];
+    uint8_t reciveData[4];
+
 private:
+    HardwareSerial *upper;
+
+    uint8_t recv_num[4];
+
 };
 
 #endif
